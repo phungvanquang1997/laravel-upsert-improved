a7d6237e1ad5d8cbd61d2198c2c461880002296e
Phùng Văn Quang <36328832+quanggpv@users.noreply.github.com>
2023-11-05 14:09:30 +0700

    Update TestController.php

--- Controller/TestController.php
+++ Controller/TestController.php
@@ -16,19 +16,35 @@ class TestController extends Controller
                 'id' => 1,
                 'name' => 'QuangPV1',
                 'email' => 'quangpv@gmal.com1',
+                'address' => 'myaddress1',
             ],
             [
                 'id' => 2,
                 'name' => 'QuangPV2',
                 'email' => 'quangpv@gmal.com2',
+                'address' => 'myaddress2',
             ],
             [
                 'id' => 3,
                 'name' => 'QuangPV3',
                 'email' => 'quangpv@gmal.com3',
+                'address' => 'myaddress3',
             ],
         ];
+      
+       $index = ['id'] // update by id
+       $fieldsUpdate = ['name']; // just update field `name`
+       $expectFields = ['name']; // update all fields except field `name`
+
+       // Just update field `name`
+       App::make(User::class)->wantsUpsert($records, ['id'], ['name'], []); 
+      
+       // Update all field by id
+       App::make(User::class)->wantsUpsert($records, ['id'], [], []);
+     
+      // update all field except 'email'
+       App::make(User::class)->wantsUpsert($records, ['id'], [], ['email']);
+
        // or this->repository->wantsUpsert($records)
-       App::make(User::class)->wantsUpsert($records);
     }
 }
